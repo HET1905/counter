add5842a2dbc84d65a8ee0056524858c75124de8
Hetal Patel <nphetal1905@gmail.com>
2019-12-16 10:53:46 -0500

    ejecting from app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// import ReactDom from "react-dom";
+// import { render } from "@testing-library/react";
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders good", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
