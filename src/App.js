3933d170077390e4dc47af575d894540d6ad135a
Hetal Patel <nphetal1905@gmail.com>
2019-12-17 12:57:13 -0500

    setup decrement

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import React from "react";
 
 import "./App.css";
-
-function App() {
-  return (
-    <div className="App" data-test="component-app">
-      <h1>Hello world</h1>
-      <h2>Second line</h2>
-      {/* {title} */}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      err: ""
+    };
+  }
+  handelDecrement = prevState => {
+    console.log(this.state.counter);
+    if (prevState <= 0) {
+      this.setState({
+        err: "Value can not be -ve",
+        counter: 0
+      });
+    } else {
+      this.setState({
+        counter: prevState - 1
+      });
+    }
+  };
+  render() {
+    const { counter } = this.state;
+    return (
+      <div className="App" data-test="component-app">
+        <h1 data-test="counter-display">
+          Counter : {counter <= 0 ? 0 : counter}
+        </h1>
+        <h2 data-test="error-display" style={{ color: "red" }}>
+          {counter <= 0 ? "value can not be -ve" : null}
+        </h2>
+        <button
+          onClick={() =>
+            this.setState({
+              counter: counter + 1
+            })
+          }
+          data-test="button-increment"
+        >
+          Increment
+        </button>
+        <button
+          data-test="button-decrement"
+          onClick={() => this.handelDecrement(counter)}
+        >
+          Decrement
+        </button>
+        {/* {title} */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -8,8 +8,78 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders good", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+const findTestByAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+test("renders without crashing", () => {
+  // const wrapper = shallow(<App />);
+  const wrapper = setup();
+  // const appComponent = wrapper.find("[data-test='component-app']");
+  const appComponent = findTestByAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
+});
+
+test("render heading", () => {
+  const wrapper = setup();
+  const counterDisplay = findTestByAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test("render Increment button", () => {
+  const wrapper = setup();
+  const buttonIncrement = findTestByAttr(wrapper, "button-increment");
+  expect(buttonIncrement.length).toBe(1);
+});
+
+test("render Decrement Button", () => {
+  const wrapper = setup();
+  const decrementButton = findTestByAttr(wrapper, "button-decrement");
+  expect(decrementButton.length).toBe(1);
+});
+
+test("test intial counter to 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
+
+test("test button click increment to counter", () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  const buttonIncrement = findTestByAttr(wrapper, "button-increment");
+  buttonIncrement.simulate("click");
+
+  const counterDisplay = findTestByAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
+
+test("test button decrement click", () => {
+  const counter = 5;
+  const wrapper = setup(null, {
+    counter
+  });
+
+  const decrementButton = findTestByAttr(wrapper, "button-decrement");
+  decrementButton.simulate("click");
+
+  const counterDisplay = findTestByAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter - 1);
+});
+test("Greater than 0", () => {
+  const counter = 0;
+  const wrapper = setup(null, { counter });
+
+  const decrementButton = findTestByAttr(wrapper, "button-decrement");
+  decrementButton.simulate("click");
+
+  const errorDisplay = findTestByAttr(wrapper, "error-display");
+  expect(counter).toBeGreaterThanOrEqual(0);
 });
